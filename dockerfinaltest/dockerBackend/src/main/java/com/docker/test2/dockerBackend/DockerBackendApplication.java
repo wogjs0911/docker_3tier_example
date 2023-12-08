4855ede569e6335b80e37fd25bb79266f01bbb22
wogjs0911 <wogjs0911@naver.com>
2023-12-08 23:59:57 +0900

    [feat] react+vite+docker test

---
+++ dockerfinaltest/dockerBackend/src/main/java/com/docker/test2/dockerBackend/DockerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.docker.test2.dockerBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerBackendApplication.class, args);
+	}
+
+}
--- springDockerTest/src/main/java/com/docker/test1/springDockerTest/test/TestController.java
+++ springDockerTest/src/main/java/com/docker/test1/springDockerTest/test/TestController.java
@@ -12,9 +12,11 @@ public class TestController {
     }
 
     /**
-     * docker 명령어 정리
+     * 1. docker에 SpringPrj를 jar로 만들어 올리기
+     *   - docker 명령어 정리
       */
-    // 1) docker build -t dockertest .
-    // 2) gradle에서 build -> clean -> build -> bootJar 이후 3) 진행
-    // 3) docker run -p 8080:8080 dockertest
+    // 1) 서버 빌드(초기화) : gradle에서 build -> clean -> build -> bootJar 이후 3) 진행
+    // 2) Dockerfile의 이미지 생성 : docker build -t dockertest .
+    // 3) 이미지 실행하여 컨테이너 생성 및 실행 : docker run -p 8080:8080 dockertest
+    // ** 해당 jar 파일은 프로젝트명을 따라간다.
 }
